4ac3f7efef1a561511198aed53345ba9a4ef7f23
Danya <viperpoison@mail.ru>
2018-04-27 22:49:30 +0300

    Start

---
+++ app/src/main/java/com/example/viper_000/pranks/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.viper_000.pranks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //<< this
+        setContentView(R.layout.activity_main);
+
+    }
+}
