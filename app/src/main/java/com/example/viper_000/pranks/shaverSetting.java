8e0725910834fd38f41f634ee332dd6052c7712f
Danya <viperpoison@mail.ru>
2018-05-05 23:52:02 +0300

    font needs finish

---
+++ app/src/main/java/com/example/viper_000/pranks/shaverSetting.java
@@ -0,0 +1,14 @@
+package com.example.viper_000.pranks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class shaverSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_shaver_setting);
+    }
+}
